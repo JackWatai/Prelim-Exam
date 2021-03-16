26af82ebd0f2b69b19b09cafe53617122acad08a
Jack Watai <jack.watai4444@gmail.com>
2021-03-16 15:17:36 +0800

    class name Student

---
+++ Student.java
@@ -0,0 +1,48 @@
+
+public class Student {
+
+	private int studentID;
+	int creds;
+	int points;
+	double gpa;
+	
+public void setID(int ID)
+{
+	ID = studentID;
+}
+public int getID()
+{
+	return studentID;
+}
+public void setcreds(int cred)
+{
+	cred = creds;
+}
+public int getcred()
+{
+	return creds;
+}
+public void setpoints(int point)
+{
+	point = points;
+}
+public int getpoints() 
+{
+	return points;
+}
+public double calcGPA(double gradepoint)
+{
+	gradepoint = points/creds;
+	return gradepoint;
+}
+
+public void printMessage()
+{
+	System.out.println("Student ID: " + studentID);
+	System.out.println("Credits: " + creds);
+	System.out.println("Points: " + points);
+	System.out.println("GPA: " + gpa);
+}
+
+
+}
