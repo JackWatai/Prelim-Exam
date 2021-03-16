ee4b6de8480f92bc56910bf95a4cabe246fd4b1d
Jack Watai <jack.watai4444@gmail.com>
2021-03-16 15:18:11 +0800

    class name ShowStudent

---
+++ ShowStudent.java
@@ -0,0 +1,16 @@
+
+public class ShowStudent {
+public static void main (String[]args) {
+	
+	Student student1 = new Student();
+	student1.setcreds(4);
+	student1.setID(1);
+	student1.setpoints(4);
+	double gradepoint = student1.getpoints();
+	
+	System.out.println("Student ID: " + student1.getID());
+	System.out.println("Credits: " + student1.getcred());
+	System.out.println("Points: " + student1.getpoints());
+	System.out.println("GPA: " + gradepoint);
+}
+}
